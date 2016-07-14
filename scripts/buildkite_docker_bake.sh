d61d41158a24faf9e11d24fb896b7a3e357247ad
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-07-14 16:24:22 -0700

    Update buildkite_docker_bake.sh

--- scripts/buildkite_docker_bake.sh
+++ scripts/buildkite_docker_bake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-buildkite-agent artifact download build/distributions/*.deb . --step gradlebuild 
+buildkite-agent artifact download build/distributions/*.deb .
 docker build -t docker_sprokest -f src/main/docker/Docker-buildkite
