58770853d37edcaf71b6e1356141964ef95c74d2
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-07-15 10:04:16 -0700

    Update buildkite_docker_bake.sh

--- scripts/buildkite_docker_bake.sh
+++ scripts/buildkite_docker_bake.sh
@@ -2,4 +2,5 @@
 buildkite-agent artifact download build/distributions/*.deb .
 eval $(docker-machine env default)
 docker build -t docker_sprokest -f src/main/docker/Docker-buildkite .
+docker tag docker_sprokest gcr.io/hopeful-list-135723/sprokest
 gcloud docker push gcr.io/hopeful-list-135723/sprokest
